6270b3d49aa3908f1bbbfbcc7fff6cbf3d396c99
Nguyễn Hải Đăng <nguyenhaidang1162005@gmail.com>
2025-02-17 17:02:53 +0700

    Add project files.

---
+++ Bai10_BangCuuChuong/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace Bai10_BangCuuChuong
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtSo = new System.Windows.Forms.TextBox();
+            this.btnTinh = new System.Windows.Forms.Button();
+            this.lbCC = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(62, 78);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // txtSo
+            // 
+            this.txtSo.Location = new System.Drawing.Point(130, 75);
+            this.txtSo.Name = "txtSo";
+            this.txtSo.Size = new System.Drawing.Size(164, 20);
+            this.txtSo.TabIndex = 1;
+            // 
+            // btnTinh
+            // 
+            this.btnTinh.Location = new System.Drawing.Point(366, 62);
+            this.btnTinh.Name = "btnTinh";
+            this.btnTinh.Size = new System.Drawing.Size(116, 44);
+            this.btnTinh.TabIndex = 2;
+            this.btnTinh.Text = "button1";
+            this.btnTinh.UseVisualStyleBackColor = true;
+            this.btnTinh.Click += new System.EventHandler(this.btnTinh_Click);
+            // 
+            // lbCC
+            // 
+            this.lbCC.FormattingEnabled = true;
+            this.lbCC.Location = new System.Drawing.Point(130, 112);
+            this.lbCC.Name = "lbCC";
+            this.lbCC.Size = new System.Drawing.Size(349, 277);
+            this.lbCC.TabIndex = 3;
+            this.lbCC.SelectedIndexChanged += new System.EventHandler(this.lsbBCC_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lbCC);
+            this.Controls.Add(this.btnTinh);
+            this.Controls.Add(this.txtSo);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtSo;
+        private System.Windows.Forms.Button btnTinh;
+        private System.Windows.Forms.ListBox lbCC;
+    }
+}
+
---
+++ Bai10_BangCuuChuong/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Bai10_BangCuuChuong
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lsbBCC_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnTinh_Click(object sender, EventArgs e)
+        {
+           
+int so_nhan;
+int so = Convert.ToInt32(txtSo.Text);
+so = 2;
+do
+{
+    so_nhan = 1;
+    do
+    {
+        lbCC.Items.Add(so + "x" + so_nhan + "=" + so * so_nhan);
+        so_nhan++;
+    }
+    while (so_nhan <= 10);
+    lbCC.Items.Add(so + "x" + so_nhan + "=" + so * so_nhan);
+    {
+    }
+    so_nhan++;
+} while (so <= 9);
+        }
+    }
+}
